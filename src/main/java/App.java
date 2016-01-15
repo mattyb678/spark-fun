e11b8fef9de515699e0718fe040bb676891764a0
Matt Berteaux <mattyb678@gmail.com>
2016-01-14 20:26:37 -0700

    Added a feed request object, which contains information about feeding a specific animal

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import model.FeedRequest;
 import model.Zoo;
 import services.ZooService;
 
@@ -11,7 +13,9 @@ public class App {
         System.out.println("App started");
 
         ZooService service = new ZooService();
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder()
+                .setDateFormat("MM/dd/yyyy")
+                .create();
 
         get("/zoo/:zooUUID", (req, resp) -> {
             return service.getZoo(UUID.fromString(req.params(":zooUUID")));
@@ -25,6 +29,16 @@ public class App {
             Zoo zoo = service.getZoo(UUID.fromString(req.params(":zooUUID")));
             if (zoo != null) {
                 zoo.addFeed(req.params(":feedName"), Integer.parseInt(req.params(":amount")));
+                return zoo;
+            }
+            return null;
+        }, gson::toJson);
+
+        post("/zoo/:zooUUID/feed", (req, resp) -> {
+            Zoo zoo = service.getZoo(UUID.fromString(req.params(":zooUUID")));
+            FeedRequest feedRequest = gson.fromJson(req.body(), FeedRequest.class);
+            if (zoo != null) {
+
                 return zoo;
             }
             return null;
---
+++ src/main/java/model/FeedRequest.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.Date;
+
+public class FeedRequest {
+    private Date date;
+    private int amount;
+    private String animal;
+    private String food;
+}
