a2d812b17fbd754fdd2a84eea61332144d3f3f8a
Matt Berteaux <mattyb678@gmail.com>
2016-01-14 19:49:19 -0700

    Added a couple routes to create a zoo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,23 @@
+import com.google.gson.Gson;
+import services.ZooService;
+
+import java.util.UUID;
+
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("App started");
+
+        ZooService service = new ZooService();
+        Gson gson = new Gson();
+
+        get("/zoo/:zooUUID", (req, resp) -> {
+            return service.getZoo(UUID.fromString(req.params(":zooUUID")));
+        }, gson::toJson);
+
+        put("/zoo/:zooName", (req, resp) -> {
+            return service.createZoo(req.params(":zooName"));
+        });
     }
 }
